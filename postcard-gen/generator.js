9d5def7a585bd78d1012e1f0dc8eb63c0c30b02c
ilyabaykalov <ilyabaykalov@icloud.com>
2023-05-23 16:41:59 +0300

    добавлена загрузка pdf

--- postcard-gen/generator.js
+++ postcard-gen/generator.js
@@ -1,3 +1,5 @@
+// window.jsPDF = window.jspdf.jsPDF;
+
 const onLoadGenerator = () => {
   if (confirm('Загрузить последние данные?')) {
     const data = JSON.parse(localStorage.getItem('data'));
@@ -51,4 +53,61 @@ const fillTemplate = () => {
     .slice(0, -3);
 
   this.document.getElementById('date').textContent = `г. Москва, ${ formattedDate } года`;
+
+  setTimeout(onDownload, 300);
 };
+
+const onDownload = () => {
+  const printForm = document.querySelector(".page");
+
+  html2pdf(printForm, {
+    filename: 'test.pdf',
+    image: {type: 'png'},
+    html2canvas: {
+      useCORS: true,
+      allowTaint: true,
+      foreignObjectRendering: true, x: 0
+    },
+    jsPDF: {
+      orientation: 'landscape',
+    }
+  });
+}
+
+// const onDownload = () => {
+//   const printForm = document.querySelector(".page");
+//
+//   const printFormWidth = printForm.innerWidth;
+//   const a4Format = [595.28, 841.89];
+//
+//   // printForm
+//   //     .set('width', ((a4Format[0] * 1.33333) - 80).toString())
+//   //     .width((a4Format[0] * 1.33333) - 80)
+//   //     .css('max-width', 'none')
+//   //     .css('width', (a4Format[0] * 1.33333) - 80);
+//
+//   html2canvas(printForm, {
+//     // useCORS: true,
+//     // imageTimeout: 10000,
+//     allowTaint: true,
+//     foreignObjectRendering: true,
+//     // removeContainer: true,
+//     // width: printFormWidth,
+//     // windowWidth: printFormWidth,
+//     scrollX: -window.scrollX,
+//     scrollY: -window.scrollY,
+//     windowWidth: document.documentElement.offsetWidth,
+//     windowHeight: document.documentElement.offsetHeight
+//   }).then((canvas) => {
+//     const img = canvas.toDataURL('image/png', 1.0);
+//     const doc = new jsPDF({
+//       // unit: 'mm',
+//       format: 'a4',
+//       orientation: 'landscape',
+//     });
+//
+//     doc.addImage(img, 'png', 20, 20);
+//     doc.save('test.pdf');
+//     // printForm.width(printFormWidth);
+//   });
+// }
