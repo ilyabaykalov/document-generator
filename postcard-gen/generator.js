97ab7d6f028f5e8318142c946ce249ace891f8fc
ilyabaykalov <ilyabaykalov@icloud.com>
2023-05-23 16:41:59 +0300

    добавлена загрузка pdf

--- postcard-gen/generator.js
+++ postcard-gen/generator.js
@@ -1,3 +1,5 @@
+// window.jsPDF = window.jspdf.jsPDF;
+
 const onLoadGenerator = () => {
   if (confirm('Загрузить последние данные?')) {
     const data = JSON.parse(localStorage.getItem('data'));
@@ -32,8 +34,9 @@ const fillTemplate = () => {
   const { full_name, date } = data;
 
   const [lastName, name, middleName] = full_name.split(' ');
+  const initials = `${ lastName } ${ name[0] }.${ middleName[0] }.`
 
-  this.document.title = `Поздравление ${ lastName } ${ name[0] }.${ middleName[0] }.`;
+  this.document.title = `Поздравление ${ initials }`;
 
   this.document.getElementById('last-name').textContent = lastName;
   this.document.getElementById('name').textContent = `${ name } ${ middleName }`;
@@ -51,4 +54,36 @@ const fillTemplate = () => {
     .slice(0, -3);
 
   this.document.getElementById('date').textContent = `г. Москва, ${ formattedDate } года`;
+
+  setTimeout(onDownload.bind(null, initials), 300);
 };
+
+const onDownload = (initials) => {
+  const printForm = document.querySelector(".page");
+
+  const options = {
+    filename: `${initials}.pdf`,
+    image: { type: 'png' },
+    html2canvas: {
+      useCORS: true,
+      allowTaint: true,
+      foreignObjectRendering: true,
+      x: 0
+    },
+    jsPDF: {
+      orientation: 'landscape',
+    }
+  };
+
+  html2pdf()
+      .set(options)
+      .from(printForm)
+      .toPdf()
+      .get('pdf')
+      .then(pdf => {
+        const view = window.open(pdf.output('bloburl'), '_blank');
+        setTimeout(() => {
+          view.document.title = initials;
+        }, 100);
+      });
+}
